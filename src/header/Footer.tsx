af1d6b7d87591affc51dd96e43b899a18b591452
Paul Mayer <p@mayer-zuffenhausen.de>
2022-07-27 18:53:52 +0200

    added LinkedIn in footer

--- src/header/Footer.tsx
+++ src/header/Footer.tsx
@@ -1,10 +1,14 @@
 import "./Footer.css";
+import instagram_logo from "../logos/Instagram_Glyph_Black.png";
+import linkedin_logo from "../logos/LinkedIn.png";
+import github_logo from "../logos/GitHub.png";
 
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="https://www.github.com/paluthebear/"><img alt="Link Github" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/></a>
-            <a href="https://www.instagram.com/palu.myr/" style={{scale: "0.8"}}><img alt="Link Instagram" src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"/></a>
+            <a href="https://www.github.com/paluthebear/"><img alt="Link Github" src={github_logo} /></a>
+            <a href="https://www.instagram.com/palu.myr/" style={{scale: "0.8"}}><img alt="Link Instagram" src={instagram_logo}/></a>
+            <a href="https://www.linkedin.com/in/paul-myr/" ><img alt="Link Linkedi=In" src={linkedin_logo} /></a>
         </div>
     )
 }
