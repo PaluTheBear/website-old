d21107a355009a36541f49291b920a4e2654f7c5
Paul Mayer <p@mayer-zuffenhausen.de>
2022-07-27 18:18:13 +0200

    vita erweitert

--- src/about/Contact.tsx
+++ src/about/Contact.tsx
@@ -12,8 +12,10 @@ const Contact = () => {
                 {() => <>
                     <h2>Vita</h2>
                     <p>From 2020-2022 worked as Software Engineering Intern at <a href="https://www.iteratec.com">Iteratec</a>, enhancing clients creating modern digital solutions using Java, TypeScript, Python and various other technologies and frameworks. </p>
-                    <p>Univeristy degree B.Sc. Computer Science estimated finish in spring 2023</p>
-                    <p>Allgemeine Hochschulreife Abitur 2017.</p>
+                    <p>Teaching 'Theoretische Informatik' for 2 terms as a tutor.</p>
+                    <p>Univeristy degree B.Sc. Computer Science estimated finish in spring 2023.</p>
+                    <p>Acquired 'Allgemeine Hochschulreife Abitur' in 2017.</p>
+                    <p>I love climbing. I also work part time in the climbing gym <a href="https://www.kletterzentrum-stuttgart.de">Kletterzentrum Stuttgart</a>, supporting wherever I can.</p>
                 </>}
             </TextTile>
         </div>
