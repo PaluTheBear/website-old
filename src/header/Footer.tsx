7e62a0f971cbb3bd7078d9d17575982899afde63
Paul Mayer <p@mayer-zuffenhausen.de>
2022-07-27 22:37:48 +0200

    adjusted footer

--- src/header/Footer.tsx
+++ src/header/Footer.tsx
@@ -6,9 +6,9 @@ import github_logo from "../logos/GitHub.png";
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="https://www.github.com/paluthebear/"><img alt="Link Github" src={github_logo} /></a>
-            <a href="https://www.instagram.com/palu.myr/" style={{scale: "0.8"}}><img alt="Link Instagram" src={instagram_logo}/></a>
-            <a href="https://www.linkedin.com/in/paul-myr/" ><img alt="Link Linkedi=In" src={linkedin_logo} /></a>
+            <a href="https://www.github.com/paluthebear/" style={{scale: "1"}}><img alt="Link Github" src={github_logo} /></a>
+            <a href="https://www.instagram.com/palu.myr/" style={{scale: "1"}}><img alt="Link Instagram" src={instagram_logo}/></a>
+            <a href="https://www.linkedin.com/in/paul-myr/" style={{scale: "1.1"}}><img alt="Link Linkedi=In" src={linkedin_logo} /></a>
         </div>
     )
 }
